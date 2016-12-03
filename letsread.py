8c9f8286dd7d9d85a1c9cb1f5c1f5143282debfd
unknown <>
2016-12-03 15:53:14 +0900

    add gitignore and sample schema

--- letsread.py
+++ letsread.py
@@ -51,9 +51,9 @@ def close_db(error):
 
 @app.route('/')
 def show_entries():
-    #db = get_db()
-    #cur = db.execute('select title, text from entries order by id desc')
-    #entries = cur.fetchall()
+    db = get_db()
+    cur = db.execute('select title, text from entries order by id desc')
+    entries = cur.fetchall()
     return render_template('show_entries.html', entries=entries)
 
 @app.route('/add', methods=['POST'])
