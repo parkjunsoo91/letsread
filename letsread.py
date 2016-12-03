b60bbbdc97449a0fd3c344d2e68fe613da21e53b
unknown <>
2016-12-03 17:28:01 +0900

    dfsfsdf

--- letsread.py
+++ letsread.py
@@ -9,30 +9,18 @@ app.config.from_object(__name__)
 
 # Load default config and override config from an environment variable
 app.config.update(dict(
-    DATABASE=os.path.join(app.root_path, 'flaskr.db'),
+    DATABASE=os.path.join(app.root_path, 'letsread.db'),
     SECRET_KEY='development key',
     USERNAME='admin',
     PASSWORD='1234'
 ))
 app.config.from_envvar('LETSREAD_SETTINGS', silent=True)
 
-def connect_db():
-    """Connects to the specific database."""
-    rv = sqlite3.connect(app.config['DATABASE'])
-    rv.row_factory = sqlite3.Row
-    return rv
-
 def init_db():
     db = get_db()
     with app.open_resource('schema.sql', mode='r') as f:
         db.cursor().executescript(f.read())
     db.commit()
-
-@app.cli.command('initdb')
-def initdb_command():
-    """Initializes the database."""
-    init_db()
-    print 'Initialized the database.'
 
 def get_db():
     """Opens a new database connection if there is none yet for the
@@ -42,6 +30,18 @@ def get_db():
         g.sqlite_db = connect_db()
     return g.sqlite_db
 
+def connect_db():
+    """Connects to the specific database."""
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+
+@app.cli.command('initdb')
+def initdb_command():
+    """Initializes the database."""
+    init_db()
+    print 'Initialized the database.'
+
 @app.teardown_appcontext
 def close_db(error):
     """Closes the database again at the end of the request."""
@@ -49,13 +49,23 @@ def close_db(error):
         g.sqlite_db.close()
 
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
+def show():
+    print(request)
+    if request.method == 'POST':
+        print(request.form['uid'])
+        print(request.form['pid'])
+        print(request.form['high'])
+
+    return render_template('view.html')
+
+'''
 def show_entries():
     db = get_db()
     cur = db.execute('select title, text from entries order by id desc')
     entries = cur.fetchall()
     return render_template('reader.html', entries=entries)
-
+'''
 @app.route('/add', methods=['POST'])
 def add_entry():
     if not session.get('logged_in'):
--- schema.sql
+++ schema.sql
@@ -1,8 +1,13 @@
-drop table if exists entries;
-create table entries (
+drop table if exists highlights;
+create table highlights (
   id integer primary key autoincrement,
   uid integer not null,
   pid integer not null,
   nodeid integer not null,
   'text' text
+);
+create table accounts (
+  id integer primary key autoincrement,
+  name char(255),
+  pw char (255)
 );
