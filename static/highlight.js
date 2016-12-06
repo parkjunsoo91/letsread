4d23fe70ede05baecfe577f7f8adafe43cdb1719
심영보 <s1luet25@gmail.com>
2016-12-06 19:43:24 +0900

    Add new highlight methods(미완성)

--- static/highlight.js
+++ static/highlight.js
@@ -1,4 +1,13 @@
-var Content;
+var Content = { p1: "We explore the use of modern recommender system technology to address the problem of learning software applications. Before describing our new command recommender system, we first define relevant design considerations. We then discuss a 3 month user study we conducted with professional users to evaluate our algorithms which generated customized recommendations for each user. Analysis shows that our item-based collaborative filtering algorithm generates 2.1 times as many good suggestions as existing techniques. In addition we present a prototype user interface to ambiently present command recommendations to users, which has received promising initial user feedback.", p2: "Many of today’s programs have not hundreds, but thousands of commands for a user to become aware of and learn [18]. In each release, more commands might be added, and without explicit effort on the part of the user to learn about new functionality, they are left untouched. For example, in Autodesk's AutoCAD, the number of commands has being growing linearly over time. And even with the thousands of commands available in AutoCAD, the largest group of users only use between 31 and 40 of them (Figure 1).", p3: "An inherent challenge with such systems is a user’s awareness [14, 39] of the functionality which is relevant to their specific goals and needs. Awareness of functionality is not only important for learning how to accomplish new tasks, but also learning how to better accomplish existing tasks. In a potential “best case scenario”, the user works with an expert next to them, who can recommend commands when appropriate.", p4: "While previous HCI literature has looked at intelligent online agents, most of this work is focused on predicting what current state a user is in, if they require assistance, and how", p5: "to overcome problems [4, 8, 9, 15, 17, 20, 31]. To our knowledge, here are few examples of systems specifically focused on recommending new commands to users [24, 25]. Furthermore, such work has never been thoroughly implemented or evaluated, and has important limitations.", p6: "Systems which recommend content to users, known as “recommender systems” are very popular today in other domains. Some of the most popular movie, shopping, and music websites provide users with personalized recommendations [23, 29, 34, 36], and research in improving recommendation algorithms is an active field of research [2]. In this paper we introduce and investigate the application of modern recommender system algorithms to address the ommand awareness problem in software applications. ", p7: "Our new system, CommunityCommands, collects usage data from a software system’s user community, and applies recommender system algorithms to generate personalized command recommendations to each user. With CommunityCommands we hope to expose users to commands they are not currently familiar with that will help them use the software more effectively. The recommended commands are displayed in a peripheral tool palette within the user interface that the user to refer to when convenient. Thus, the system is much more ambient in nature compared to online agents such as “Clippy” or even simple techniques like “Tip of the Day”. After discussing implementation details, we describe a 3 month evaluation of our recommender system algorithms, conducted with real users. Our new algorithms provided significantly improved recommendations in comparison to existing approaches." };;
+var obj = {
+    "p1":[{"start":0,"end":0,"style":"highlight"},{"start":673,"end":673,"style":"highlight"}],
+    "p2":[{"start":0,"end":0,"style":"highlight"},{"start":508,"end":508,"style":"highlight"}],
+    "p3":[{"start":0,"end":0,"style":"highlight"},{"start":422,"end":422,"style":"highlight"}],
+    "p4":[{"start":0,"end":0,"style":"highlight"},{"start":183,"end":183,"style":"highlight"}],
+    "p5":[{"start":0,"end":0,"style":"highlight"},{"start":274,"end":274,"style":"highlight"}],
+    "p6":[{"start":0,"end":0,"style":"highlight"},{"start":495,"end":495,"style":"highlight"}],
+    "p7":[{"start":0,"end":0,"style":"highlight"},{"start":873,"end":873,"style":"highlight"}],
+};
 
 function loadContents() {
     //Hardcoded... 실제 논문 내용들
@@ -76,6 +85,80 @@ function spanToJSON(pid) {
         console.log('{"start":' + startIdx + ',"end":' + endIdx + '}');
         startIdx = spannedHTML.indexOf('<span class="highlight">');
     }
+}
+
+//JSON 기반으로 template HTML에 highlight넣어주는 함수
+function highlightJSON(pid) {
+    var len = obj[pid].length;
+    console.log("length: " + len);
+    var innerHTML = Content[pid];
+    for (j = len - 2; j >= 0; j--) {
+        var start = obj[pid][j].start;
+        var end = obj[pid][j].end;
+        innerHTML = innerHTML.substring(0, start) + "<span class='highlight'>" + innerHTML.substring(start, end) + "</span>" + innerHTML.substring(end);
+    }
+    document.getElementById(pid).innerHTML = innerHTML;
+}
+//마우스 드래그로 선택시 JSON으로 변환해주는 함수
+function selectToHighlight() {
+    var sel = window.getSelection();
+    var range = sel.getRangeAt(0);
+    var pid = sel.anchorNode.parentElement.id;
+    // var element = sel.anchorNode.parentElement;
+    // var id = element.id;
+    // while (element.tagName != 'P') {
+    //     element = element.parentElement;
+    //     id = element.id;
+    // }
+    console.log(range.startContainer.childNodes);
+
+    var selStart = range.startOffset-1;
+    var selEnd = range.endOffset-1;
+    if (selEnd > Content[pid].length) {
+        selEnd = Content[pid].length;
+    }
+    var spliceIdx = 0; //splice시작하는 인덱스
+    var spliceLength = 0; //새로운 항목 추가를 위해 삭제하는 엔트리 개수
+    var startFix = false;
+    var endFix = false;
+    var newStart = selStart;
+    var newEnd = selEnd;
+
+    console.log(selStart + " " + selEnd);
+    for (i = 0; i < obj[pid].length; i++) {
+        console.log(i + "th " + obj[pid][i].start + " " + obj[pid][i].end);
+        if (!startFix && obj[pid][i].start <= selStart) {
+            if (obj[pid][i].end >= selStart) {
+                newStart = obj[pid][i].start;
+            }
+        } else if (!startFix && obj[pid][i].start > selStart) {
+            startFix = true;
+        }
+        if (!endFix && obj[pid][i].end >= selEnd) {
+            if (obj[pid][i].start <= selEnd) {
+                newEnd = obj[pid][i].end;
+            }
+            endFix = true;
+        }
+    }
+    startFix = false;
+    for (i = 0; i < obj[pid].length; i++) {
+        if (!startFix && newStart <= obj[pid][i].start) {
+            spliceIdx = i;
+            startFix = true;
+        }
+        if (newEnd >= obj[pid][i].end) {
+            spliceLength = i + 1;
+        }
+    }
+    spliceLength -= spliceIdx;
+
+    obj[pid].splice(spliceIdx, spliceLength, {
+        start: newStart,
+        end: newEnd
+    });
+    console.log(spliceIdx + " " + spliceLength + " " + newStart + " " + newEnd + " " + obj[pid].length);
+    highlightJSON(pid);    
 }
 
 function highlighter() {
